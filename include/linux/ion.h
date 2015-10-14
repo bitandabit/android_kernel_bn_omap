3663d5e3d5781ada4a62b12cb060e2c8a972f7a5
myfluxi <linflux@arcor.de>
2015-10-14 20:49:38 +0200

    ion: Add ion_user_handle_t to make userspace happy
    Change-Id: If291c13d02274eb8200090254dac1a6a969a32e1

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -20,6 +20,7 @@
 #include <linux/types.h>
 
 struct ion_handle;
+typedef struct ion_handle *ion_user_handle_t;
 /**
  * enum ion_heap_types - list of all possible types of heaps
  * @ION_HEAP_TYPE_SYSTEM:	 memory allocated via vmalloc
@@ -267,7 +268,7 @@ struct ion_allocation_data {
 	size_t len;
 	size_t align;
 	unsigned int flags;
-	struct ion_handle *handle;
+	ion_user_handle_t handle;
 };
 
 /**
@@ -282,7 +283,7 @@ struct ion_allocation_data {
  * provides the file descriptor and the kernel returns the handle.
  */
 struct ion_fd_data {
-	struct ion_handle *handle;
+	ion_user_handle_t handle;
 	int fd;
 	unsigned char cacheable;
 };
@@ -292,7 +293,7 @@ struct ion_fd_data {
  * @handle:	a handle
  */
 struct ion_handle_data {
-	struct ion_handle *handle;
+	ion_user_handle_t handle;
 };
 
 /**
