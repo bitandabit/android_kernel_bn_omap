583d63aa5fd7e13a9dadef4d27a5d1698583defa
Luden <luden@ghostmail.com>
2016-02-15 00:21:20 +0100

    Added IOBUFS max size monitoring.
    Added debugging code to monitor the max size requested for IOBUFS
    buffers.
    
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>
    
    Conflicts:
    	drivers/rpmsg/rpmsg_omx.c
    
    Change-Id: I954b357c506b6e19012bc94a65d00d9e86b83c58

--- drivers/rpmsg/rpmsg_omx.c
+++ drivers/rpmsg/rpmsg_omx.c
@@ -120,6 +120,18 @@ static LIST_HEAD(rpmsg_omx_services_list);
 #endif
 #endif
 
+/*
+ * TEMP: Should lookup with rproc, but these values are the same
+ * as currently defined
+ */
+#define TILER_START	0x60000000
+#define TILER_END	0x80000000
+#define ION_1D_START	0xBA300000
+#define ION_1D_END	0xBFD00000
+#define ION_1D_VA	0x88000000
+
+static u32 max_iobufs_addr = ION_1D_VA;
+
 static int _rpmsg_pa_to_da(struct rpmsg_omx_instance *omx, u32 pa, u32 *da)
 {
 	int ret;
@@ -347,6 +359,15 @@ static int _rpmsg_omx_map_buf(struct rpmsg_omx_instance *omx, char *packet)
 				*buffer = da;
 		}
 	}
+
+#ifdef CONFIG_CMA_DEBUG
+	if (da > TILER_END && da < ION_1D_VA + ION_1D_END - ION_1D_START) {
+		if (da > max_iobufs_addr) {
+			max_iobufs_addr = da;
+			pr_err("new max IOBUFS addr is 0x%x, max size is %dMB\n", max_iobufs_addr, (max_iobufs_addr - ION_1D_VA) / SZ_1M);
+		}
+	}
+#endif
 
 	return ret;
 }
