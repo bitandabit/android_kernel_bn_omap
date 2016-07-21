2ea7dda0785daee02fdcb90ce093654dfaf2b726
D. Andrei Măceș <dmaces@nd.edu>
2016-07-21 10:57:21 -0400

    Revert "Input: MT - Add flags to input_mt_init_slots()"
    This reverts commit 2cff79bde8f4fe971b29aa5046c20e00517c4ebe.

--- drivers/input/input-mt.c
+++ drivers/input/input-mt.c
@@ -25,8 +25,7 @@
  * May be called repeatedly. Returns -EINVAL if attempting to
  * reinitialize with a different number of slots.
  */
-int input_mt_init_slots(struct input_dev *dev, unsigned int num_slots,
-			unsigned int flags)
+int input_mt_init_slots(struct input_dev *dev, unsigned int num_slots)
 {
 	struct input_mt *mt = dev->mt;
 	int i;
@@ -41,7 +40,6 @@ int input_mt_init_slots(struct input_dev *dev, unsigned int num_slots,
 		return -ENOMEM;
 
 	mt->num_slots = num_slots;
-	mt->flags = flags;
 	input_set_abs_params(dev, ABS_MT_SLOT, 0, num_slots - 1, 0, 0);
 	input_set_abs_params(dev, ABS_MT_TRACKING_ID, 0, TRKID_MAX, 0, 0);
 
